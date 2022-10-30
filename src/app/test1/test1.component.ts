17b4b1399bb3c92c4143a9d33a6e82910c3d8013
DaniilRalf <ralf.danya@mail.ru>
2022-10-30 20:09:09 +0700

    operators

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,29 +1,38 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs';
+import {map, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-test1',
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.css']
 })
+
+
+// Observable - наблюдаемый обьект, который мы отслеживаем "Конструктор источника событий"
+
+// subscribe - подписка на .источник события для отслеживания изменения
+
+// Observer - Наблюдатель, это потребитель значений, предоставляемых Observable.
+// Наблюдатели, это просто набор обратных вызовов, по одному для каждого типа уведомления, доставляемого Observable: next, error, и complete.
+
+
+
 export class Test1Component implements OnInit, AfterViewInit {
   @ViewChild('inputTest')
   public inputTest!: ElementRef;
 
-
   public observTest$ = new Observable(sub => {
     this.inputTest.nativeElement.addEventListener('input', (value: any) => {
-      sub.next(value)
+      sub.next(value) //когда мы слушаем данный Обсервбл мы получаем значение value
     });
-  }); 
-  
+  });
+
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit(): void { //вызываем подписку именно тут только для того чтобы инициализировались данные ДОМ
     this.observTest$.subscribe(qqq => {
       console.log(qqq);
     })
---
+++ src/app/test2/test2.component.ts
@@ -0,0 +1,40 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {debounceTime, fromEvent, map, mergeMap, switchMap, tap} from "rxjs";
+import {ajax} from "rxjs/internal/ajax/ajax";
+
+@Component({
+  selector: 'app-test2',
+  templateUrl: './test2.component.html',
+  styleUrls: ['./test2.component.css']
+})
+export class Test2Component implements OnInit, AfterViewInit {
+  public url = 'https://api.github.com/search/users?q=';
+  @ViewChild('new_input') public new_input!: ElementRef;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngAfterViewInit() {
+    let stream$ = fromEvent(this.new_input.nativeElement, 'input')
+      .pipe(                              //указываем что результат наблюдаемого обьекта мы будем изенять
+        map((item: any) => {       //изменяем каждый элемент слушаемого обьекта
+          return item.target.value;
+        }),                               //если нам надо сделать несколько обработок указываем их через зяпяту.
+        debounceTime(1000),       //не стримим пока не пройдет 1 секунда после завершения ввода
+        switchMap(inputValue => {  //switchMap создает внутрисебя новый Observable и передает в него параметры из прошлого
+          return ajax.getJSON(this.url + (inputValue || '***'))  //ajax это как раз встроенна ф-ция rxjs типа Observable
+        }),
+        map((response: any) => {
+          return response.items
+        }),
+        tap(console.log),             //пространство для внешних манипуляций
+        // mergeMap(item => item),    //можноиспользовать для того что бы выводить все не оним массивом а отдельно каждый элемент
+      )
+
+    stream$.subscribe((i) => {
+      console.log(i);
+    })
+  }
+
+}
